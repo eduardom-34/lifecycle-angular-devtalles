62f556a59f702996b74982c1293f0a1af5009395
CesarLinux <eduardomelchor34@gmail.com>
2025-01-09 19:24:37 -0600

    first commit

---
+++ src/app/products/components/price/price.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'products-price',
+  standalone: false,
+
+  templateUrl: './price.component.html',
+  styleUrl: './price.component.css'
+})
+export class PriceComponent {
+
+}
